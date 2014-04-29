16e26a412bcdff0cc4e98a79084078677e414594
Paul Ingles <paul@oobaloo.co.uk>
2014-04-29 19:02:00 +0100

    add sources and sinks

--- src/feedback/processes.clj
+++ src/feedback/processes.clj
@@ -4,6 +4,21 @@
   (:import [clojure.lang IFn]
            [clojure.core.async.impl.protocols WritePort ReadPort]))
 
+
+(defrecord ASource [out-ch f]
+    IFn
+    (invoke [_] (f))
+    ReadPort
+    (take! [this handler]
+      (p/take! out-ch handler)))
+
+(defrecord ASink [in-ch f]
+    IFn
+    (invoke [_] (f))
+    WritePort
+    (put! [port val handler]
+      (p/put! in-ch val handler)))
+
 (defrecord AProcess [in-ch out-ch f]
   IFn
   (invoke [_] (f))
@@ -15,6 +30,14 @@
   (take! [this handler]
     (p/take! out-ch handler)))
 
+(defmacro defsink [name args & body]
+  `(let [in-ch# (async/chan)
+         ~args [in-ch#]]
+     (def ~name (ASink. in-ch#
+                        (fn ~(symbol (str name "-sinkfn"))
+                          []
+                          ~@body)))))
+
 (defmacro defprocess [name args & body]
   `(let [in-ch# (async/chan)
          out-ch# (async/chan)
@@ -25,6 +48,13 @@
                              []
                              ~@body)))))
 
+(defn connect
+  "Forwards messages from process to process."
+  [{:keys [out-ch] :as from} {:keys [in-ch] :as to}]
+  (async/go-loop [val (async/<! from)]
+    (when val
+      (async/>! in-ch val)
+      (recur (async/<! from)))))
 
 (comment
   (defprocess say-hello [in-ch out-ch]
@@ -32,14 +62,17 @@
       (async/>! out-ch (str "Hello " m))
       (recur (async/<! in-ch))))
 
-  (say-hello) ; start the process
-
   ;; now we describe a go-loop consuming values
   ;; from the process
-  (async/go-loop [m (async/<! say-hello)]
-    (when m
-      (println "Received:" m)
-      (recur (async/<! say-hello))))
+  (defsink print-message [in-ch]
+    (async/go-loop [m (async/<! in-ch)]
+      (when m
+        (println "Received:" m)
+        (recur (async/<! in-ch)))))
+
+  (print-message) ; start the sink
+  (say-hello) ; start our processing step
+  (connect say-hello print-message)
 
   ;; finally, lets send some values to the running process
   (async/put! say-hello "World 1")
